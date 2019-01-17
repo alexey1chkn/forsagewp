05e700a519ab3f809b7dd0a9e73d2a49fbc3c3dc
Alexey Lychkin <alexey1chkn@gmail.com>
2019-01-17 18:14:04 +0300

    Исправлен баг пагинации
    Теперь, если даже не указаны никакие атрибуты, то пагинация работает корректно

--- wp-content/themes/forsage_penza/disks_product_list.php
+++ wp-content/themes/forsage_penza/disks_product_list.php
@@ -76,14 +76,19 @@ function disks_filter(){
 
 <nav class="navigation pagination_page_down" role="navigation">
 	<?php
+		if(empty($_GET))
+			$get_sign = "?";
+		else
+			$get_sign = "&";
+
 		for($i = 1; $i <= $page_quantity; $i++){
 			if ( $_GET['page_my'] == NULL ){
 				$page = $_GET['page_my'];
 				if ($i <= 3 || $i > $page_quantity-3 || $i == $page-1 || $i == $page || $i == $page+1)
 					if( $i == $page ) 
-						echo '<a class="pagination_page_active" href="' . $_SERVER['REQUEST_URI'] . '&page_my=' .  $i . '"><span>' . $i . '</span></a>';
+						echo '<a class="pagination_page_active" href="' . $_SERVER['REQUEST_URI'] . $get_sign . 'page_my=' .  $i . '"><span>' . $i . '</span></a>';
 					else 
-						echo '<a href="' . $_SERVER['REQUEST_URI'] . '&page_my=' . $i . '"><span>' . $i . '</span></a>';
+						echo '<a href="' . $_SERVER['REQUEST_URI'] . $get_sign . 'page_my=' . $i . '"><span>' . $i . '</span></a>';
 				elseif($count < 3 ){
 						echo '<span class="pagination_page_between">.</span>';
 						$count++;
